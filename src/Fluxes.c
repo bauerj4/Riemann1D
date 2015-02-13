dbf5c4c55f89a5aadc00f10e550bf35bb6890030
Jake Bauer <bauerj4@rpi.edu>
2015-02-13 11:46:46 +0000

    Added primitive-conservative conversions.

--- src/Fluxes.c
+++ src/Fluxes.c
@@ -5,6 +5,45 @@
 
 using namespace std;
 
+/*
+  For the vector conversion routines,
+  use the equation of state:
+  
+  E = P / (rho (gamma - 1))
+*/
+
+int primitiveTo1DConservative(vector<double> &primitive)
+{
+  if (primitive.size()!=3)
+    {
+      printf("INCORRECT PRIMITIVE VARIABLE SIZE!\n");
+      return 1;
+    }
+  double gamma = 1.4;
+  primitive[1] *= primitive[0];
+  // Compute energy from pressure
+  double energy = primitive[2] / (primitive[0] * (gamma - 1.));
+  primitive[2] = energy * primitive[0];
+  return 0;
+}
+
+int conservativeTo1DPrimative(vector<double> conservative)
+{
+  double gamma = 1.4;
+
+  if (conservative.size()!=3)
+    {
+      printf("INCORRECT CONSERVATIVE VARIABLE SIZE!\n");
+      return 1;
+    }
+
+
+  conservative[1] = conservative[1] / conservative[0]; 
+  conservative[2] = conservative[2] / conservative[0];
+  conservative[2] *= conservative[0] * (gamma - 1.); //from eq of state
+
+  return 0;
+}
 
 int HLLC_FLUX(mesh_t &mesh, vector<double> &fluxes)
 {
